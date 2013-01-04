114e9978df36f8a9b1c6589a43c90c640225bd46
rlcohen <rich.cohen@i-intime.org>
2013-01-04 11:01:04 -0500

    -- Added documentation about how injection works -- Updated how we generate html so that radio button names are unique and class is "rating". This prevents interaction of css between questions,

--- app/helpers/card_questions_helper.rb
+++ app/helpers/card_questions_helper.rb
@@ -4,6 +4,11 @@ module CardQuestionsHelper
     return id + suffix.to_s
   end
 
+  def self.make_name(id)
+    return id +  "__name"
+  end
+
+
   def self.get_answer(question_id, user_id, card_id)
 
     answer = Answer.where(["question_id = ? AND user_id = ? AND card_id = ?", question_id, user_id, card_id])
