c9e57c75e6f957e6b92f4c8c89b4875fc83479f8
rlcohen <rich.cohen@i-intime.org>
2013-02-05 16:11:28 -0500

    -- another fix

--- app/helpers/util_helper.rb
+++ app/helpers/util_helper.rb
@@ -83,7 +83,7 @@ module UtilHelper
   end
 
   def self.replace_case_name_tag(text, case_name)
-    replace_quote(replace_tag(text, SUB_CASENAME, case_name))
+    replace_tag(text, SUB_CASENAME, case_name)
   end
 
   def self.replace_case_id_tag(text, case_id)
@@ -91,7 +91,7 @@ module UtilHelper
   end
 
   def self.replace_card_name_tag(text, card_name)
-    replace_quote(replace_tag(text, SUB_CARDNAME, card_name))
+    replace_tag(text, SUB_CARDNAME, card_name)
   end
 
   def self.replace_group_id_tag(text, group_id)
